f4aaf956ed390a5b6ca4c785c7f391eb2a7dadd0
Reut-Maslansky <maslanskyr@gmail.com>
2021-05-19 12:14:02 +0300

    init queue for each iterator type

--- sources/BinaryTree.hpp
+++ sources/BinaryTree.hpp
@@ -1,8 +1,15 @@
 #include <iostream>
+#include <stack>
+#include <queue>
+
 using namespace std;
 
 namespace ariel
 {
+    const int INORDER = 0;
+    const int PREORDER = 1;
+    const int POSTORDER = 2;
+
     template <typename T>
     class BinaryTree
     {
@@ -11,22 +18,23 @@ namespace ariel
         struct Node
         {
             T value;
-            Node *parent;
+            // Node *parent;
             Node *left;
             Node *right;
-            Node(const T &v, Node *parent) : value(v), parent(parent), left(nullptr), right(nullptr) {}
+            // Node(const T &v, Node *parent) : value(v), parent(parent), left(nullptr), right(nullptr) {}
+            Node(const T &v) : value(v), left(nullptr), right(nullptr) {}
 
             ~Node()
             {
                 delete left;
                 delete right;
-                delete parent;
+                // delete parent;
             }
         };
 
         Node *root;
-        BinaryTree(const BinaryTree &copy) {}
-        BinaryTree &operator=(const BinaryTree &copy) {}
+        // BinaryTree(const BinaryTree &copy) {}
+        // BinaryTree &operator=(const BinaryTree &copy) {}
 
     public:
         BinaryTree() : root(nullptr) {}
@@ -37,19 +45,22 @@ namespace ariel
 
         BinaryTree &add_root(T t)
         {
-            root = new Node{t, nullptr};
+            // root = new Node{t, nullptr};
+            root = new Node{t};
             return *this;
         }
 
         BinaryTree &add_left(T p, T v)
         {
-            root = new Node{v, nullptr};
+            // root = new Node{v, nullptr};
+            root = new Node{v};
             return *this;
         }
 
         BinaryTree &add_right(T p, T v)
         {
-            root = new Node{v, nullptr};
+            // root = new Node{v, nullptr};
+            root = new Node{v};
             return *this;
         }
 
@@ -90,9 +101,109 @@ namespace ariel
 
         private:
             Node *current_node;
+            queue<Node *> in;
+            queue<Node *> pre;
+            queue<Node *> post;
+            void init_queue_inorder()
+            {
+                if (current_node == nullptr)
+                {
+                    return;
+                }
+                stack<Node *> s;
+                Node *curr = current_node;
+
+                while (curr != nullptr || s.empty() == false)
+                {
+                    while (curr != nullptr)
+                    {
+                        s.push(curr);
+                        curr = curr->left;
+                    }
+
+                    curr = s.top();
+                    in.push(curr);
+                    s.pop();
+
+                    curr = curr->right;
+                }
+            }
+            void init_queue_preorder()
+            {
+                if (current_node == nullptr)
+                {
+                    return;
+                }
+                stack<Node *> s;
+                s.push(current_node);
+
+                while (s.empty() == false)
+                {
+                    Node *node = s.top();
+                    pre.push(node);
+                    s.pop();
+
+                    if (node->right)
+                        s.push(node->right);
+                    if (node->left)
+                        s.push(node->left);
+                }
+            }
+            void init_queue_postorder()
+            {
+                if (current_node == nullptr)
+                {
+                    return;
+                }
+                stack<Node *> s;
+                Node *node = current_node;
+
+                do
+                {
+                    while (node)
+                    {
+                        if (node->right)
+                        {
+                            s.push(node->right);
+                        }
+                        s.push(node);
+
+                        node = node->left;
+                    }
+
+                    node = s.top();
+                    s.pop();
+
+                    if (node->right && s.top() == node->right)
+                    {
+                        s.pop();
+                        s.push(node);
+                        node = node->right;
+                    }
+                    else
+                    {
+                        post.push(node);
+                        node = nullptr;
+                    }
+                } while (!s.empty());
+            }
 
         public:
-            iteratorTree(Node *ptr = nullptr) : current_node(ptr) {}
+            iteratorTree(Node *ptr = nullptr, int t = -1) : current_node(ptr)
+            {
+                if (t == INORDER)
+                {
+                    init_queue_inorder();
+                }
+                if (t == PREORDER)
+                {
+                    init_queue_preorder();
+                }
+                if (t == POSTORDER)
+                {
+                    init_queue_postorder();
+                }
+            }
 
             const T &operator*() const
             {
@@ -132,12 +243,12 @@ namespace ariel
         };
         /***************END OF CLASS ITERATOR***************/
 
-        iteratorTree begin_preorder() const { return iteratorTree(root); }
-        iteratorTree end_preorder() const { return iteratorTree(root); }
-        iteratorTree begin_inorder() const { return iteratorTree(root); }
-        iteratorTree end_inorder() const { return iteratorTree(root); }
-        iteratorTree begin_postorder() const { return iteratorTree(root); }
-        iteratorTree end_postorder() const { return iteratorTree(root); }
+        iteratorTree begin_preorder() const { return iteratorTree(root, PREORDER); }
+        iteratorTree end_preorder() const { return iteratorTree(root, PREORDER); }
+        iteratorTree begin_inorder() const { return iteratorTree(root, INORDER); }
+        iteratorTree end_inorder() const { return iteratorTree(root, INORDER); }
+        iteratorTree begin_postorder() const { return iteratorTree(root, POSTORDER); }
+        iteratorTree end_postorder() const { return iteratorTree(root, POSTORDER); }
         iteratorTree begin() const { return begin_inorder(); }
         iteratorTree end() const { return end_inorder(); }
     };
